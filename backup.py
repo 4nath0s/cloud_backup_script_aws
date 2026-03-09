d555f691b97aa7d6155be2b8ace3f9d8e14c0488
Antoine <ant.danaels@gmail.com>
2026-03-09 21:57:32 +0100

    add logging and error handling

--- backup.py
+++ backup.py
@@ -3,29 +3,50 @@ import shutil
 import datetime
 import boto3
 import dotenv
+import logging
 
-dotenv.load_dotenv()
-FOLDER_LOCATION = os.environ["DOSSIER_A_SAUVEGARDER"]
-AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
-AWS_REGION = os.environ["AWS_REGION"]
-AWS_BUCKET_NAME = os.environ["AWS_BUCKET_NAME"]
+logging.basicConfig(filename='backup.log', level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+try :
+    dotenv.load_dotenv()
+    FOLDER_LOCATION = os.environ["DOSSIER_A_SAUVEGARDER"]
+    AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+    AWS_REGION = os.environ["AWS_REGION"]
+    AWS_BUCKET_NAME = os.environ["AWS_BUCKET_NAME"]
+    logging.info("global variables loaded succesfully")
+except:
+    logging.exception("error loading global variables")
+    exit(1)
 
 
 def backup_creation(folder_localisation):
     if os.path.exists(folder_localisation):
         save_file = "save" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
         shutil.make_archive(save_file, "zip", folder_localisation)
+        logging.info("zip file created")
         return os.path.realpath(save_file + ".zip")
     else:
-        print("no folder located")
+        logging.error("no folder located")
+        exit(1)
 
     
 def upload_to_S3(zip_location):
-    s3 = boto3.client("s3", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
-    s3.upload_file(zip_location, AWS_BUCKET_NAME, os.path.basename(zip_location))
+    try :
+        s3 = boto3.client("s3", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
+        logging.info("connection established to aws")
+    except:
+        logging.exception("can't connect to aws")
+        exit(1)
+    try :
+        s3.upload_file(zip_location, AWS_BUCKET_NAME, os.path.basename(zip_location))
+        logging.info("file loaded succesfully")
+    except:
+        logging.exception("error while loading file")
+
 
 if __name__ == "__main__":
+    
     save_name = backup_creation(FOLDER_LOCATION)
     upload_to_S3(save_name)
     
