b914e37ada0c7927a54e595fe2863d0b17235be2
Antoine <ant.danaels@gmail.com>
2026-03-09 21:32:14 +0100

    cloud connection

--- backup.py
+++ backup.py
@@ -1,12 +1,32 @@
 import os
 import shutil
 import datetime
+import boto3
+import dotenv
+
+dotenv.load_dotenv()
+FOLDER_LOCATION = os.environ["DOSSIER_A_SAUVEGARDER"]
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+AWS_REGION = os.environ["AWS_REGION"]
+AWS_BUCKET_NAME = os.environ["AWS_BUCKET_NAME"]
+
 
 def backup_creation(folder_localisation):
     if os.path.exists(folder_localisation):
-        save_file = "save" + str(datetime.datetime.now())
+        save_file = "save" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
         shutil.make_archive(save_file, "zip", folder_localisation)
+        return os.path.realpath(save_file + ".zip")
     else:
         print("no folder located")
+
+    
+def upload_to_S3(zip_location):
+    s3 = boto3.client("s3", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
+    s3.upload_file(zip_location, AWS_BUCKET_NAME, os.path.basename(zip_location))
+
+if __name__ == "__main__":
+    save_name = backup_creation(FOLDER_LOCATION)
+    upload_to_S3(save_name)
     
     
